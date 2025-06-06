21bb606df9d3bb13d892fd0d817e4f7b0c96482e
JustTony98 <tonydm9886@gmail.com>
2025-06-06 14:01:30 +0300

    add var. values & consolelogs, lesson02

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "myFirstRepo";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 20000;
+let rollback = 1500;
+let fullPrice = 100000;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(
+  `Стоимость верстки экранов ${screenPrice} рублей, \nСтоимость разработки сайтов ${fullPrice} рублей`
+);
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
 
 alert("Предъявите документики");
 console.log("Документики отсутствуют");
